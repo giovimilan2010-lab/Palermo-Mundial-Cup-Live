986a14e8046046fc5fbc55c74773d3fa230ca93f
giovimilan2010-lab <giovimilan2010@gmail.com>
2026-07-23 18:37:39 +0200

    Create giocatori.js

---
+++ giocatori.js
@@ -0,0 +1,55 @@
+const giocatori = [
+
+{
+nome: "Angelo Murana",
+squadra: "Argentina",
+bandiera: "🇦🇷",
+numero: 1,
+ruolo: "Portiere",
+gol: 0,
+assist: 0,
+gialli: 0,
+rossi: 0,
+mvp: 0
+},
+
+{
+nome: "Davide Luparello",
+squadra: "Argentina",
+bandiera: "🇦🇷",
+numero: 4,
+ruolo: "Difensore",
+gol: 0,
+assist: 0,
+gialli: 0,
+rossi: 0,
+mvp: 0
+},
+
+{
+nome: "Giuseppe Ilardi",
+squadra: "Argentina",
+bandiera: "🇦🇷",
+numero: 3,
+ruolo: "Difensore",
+gol: 0,
+assist: 0,
+gialli: 0,
+rossi: 0,
+mvp: 0
+},
+
+{
+nome: "Fortunato Tripodi",
+squadra: "Messico",
+bandiera: "🇲🇽",
+numero: 77,
+ruolo: "Portiere",
+gol: 0,
+assist: 0,
+gialli: 0,
+rossi: 0,
+mvp: 0
+}
+
+];
