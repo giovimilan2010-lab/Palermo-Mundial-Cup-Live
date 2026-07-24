3a21c217183d9205ca7516a6f2db683a4c1ae919
giovimilan2010-lab <giovimilan2010@gmail.com>
2026-07-24 16:15:40 +0200

    Update mvp.js

--- mvp.js
+++ mvp.js
@@ -1,1 +1,43 @@
+const SHEET_ID = "1rQsJx7Vmeu8djzT-0e6VhYS-ODprt0MF5vTOXY96gjA";
 
+const SHEET_NAME = "PARTITA";
+
+const URL = `https://opensheet.elk.sh/${SHEET_ID}/${SHEET_NAME}`;
+
+
+async function caricaMVP(){
+
+    const risposta = await fetch(URL);
+
+    const dati = await risposta.json();
+
+    const partita = dati[0];
+
+
+    document.getElementById("giornata").innerHTML =
+    partita.Giornata;
+
+
+    document.getElementById("partita").innerHTML =
+    partita.Partita;
+
+
+    document.getElementById("giocatore1").innerHTML =
+    partita.Giocatore1;
+
+
+    document.getElementById("giocatore2").innerHTML =
+    partita.Giocatore2;
+
+
+    document.getElementById("giocatore3").innerHTML =
+    partita.Giocatore3;
+
+
+    document.getElementById("giocatore4").innerHTML =
+    partita.Giocatore4;
+
+}
+
+
+caricaMVP();
