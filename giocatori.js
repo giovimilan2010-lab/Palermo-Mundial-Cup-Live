43102a0619952d86723f2cde9cab5b6f17c8eb7b
giovimilan2010-lab <giovimilan2010@gmail.com>
2026-07-24 02:35:17 +0200

    Update giocatori.js

--- giocatori.js
+++ giocatori.js
@@ -1,55 +1,269 @@
 const giocatori = [
 
 {
-nome: "Angelo Murana",
-squadra: "Argentina",
-bandiera: "🇦🇷",
-numero: 1,
-ruolo: "Portiere",
-gol: 0,
-assist: 0,
-gialli: 0,
-rossi: 0,
-mvp: 0
+id:1,
+nome:"Angelo Murana",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:1,
+ruolo:"Portiere",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
 },
 
 {
-nome: "Davide Luparello",
-squadra: "Argentina",
-bandiera: "🇦🇷",
-numero: 4,
-ruolo: "Difensore",
-gol: 0,
-assist: 0,
-gialli: 0,
-rossi: 0,
-mvp: 0
+id:2,
+nome:"Davide Luparello",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:4,
+ruolo:"Difensore",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
 },
 
 {
-nome: "Giuseppe Ilardi",
-squadra: "Argentina",
-bandiera: "🇦🇷",
-numero: 3,
-ruolo: "Difensore",
-gol: 0,
-assist: 0,
-gialli: 0,
-rossi: 0,
-mvp: 0
+id:3,
+nome:"Giuseppe Ilardi",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:3,
+ruolo:"Difensore",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
 },
 
 {
-nome: "Fortunato Tripodi",
-squadra: "Messico",
-bandiera: "🇲🇽",
-numero: 77,
-ruolo: "Portiere",
-gol: 0,
-assist: 0,
-gialli: 0,
-rossi: 0,
-mvp: 0
+id:4,
+nome:"Pietro Intravaglia",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:5,
+ruolo:"Difensore",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:5,
+nome:"Cristian D'Aloisi",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:22,
+ruolo:"Centrocampista",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:6,
+nome:"Filippo Sicomaro",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:14,
+ruolo:"Centrocampista",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:7,
+nome:"Stevean Berlich",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:77,
+ruolo:"Centrocampista",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:8,
+nome:"Davide Pirrotta",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:10,
+ruolo:"Attaccante",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:9,
+nome:"Domenico Faraci",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:7,
+ruolo:"Attaccante",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:10,
+nome:"Samu Pucci",
+squadra:"Argentina",
+bandiera:"🇦🇷",
+numero:11,
+ruolo:"Attaccante",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:11,
+nome:"Fortunato Tripodi",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:77,
+ruolo:"Portiere",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:12,
+nome:"Francesco Mule",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:6,
+ruolo:"Difensore",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:13,
+nome:"Mattia Tusa",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:21,
+ruolo:"Difensore",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:14,
+nome:"Antonino Bartolotta",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:10,
+ruolo:"Difensore",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:15,
+nome:"Yari Mancuso",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:9,
+ruolo:"Centrocampista",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:16,
+nome:"Giuseppe Lo Cascio",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:13,
+ruolo:"Centrocampista",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:17,
+nome:"Stefano La Rosa",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:7,
+ruolo:"Centrocampista",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:18,
+nome:"Dennis Favarò",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:19,
+ruolo:"Attaccante",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
+},
+
+{
+id:19,
+nome:"Danilo Schiera",
+squadra:"Messico",
+bandiera:"🇲🇽",
+numero:12,
+ruolo:"Attaccante",
+gol:0,
+assist:0,
+gialli:0,
+rossi:0,
+mvp:0
 }
 
 ];
